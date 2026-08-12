60d3d86e2c8faff19f4766d7816519023b6c9993
vigneshwari-1205 <k.vigneshmdk2005@gmail.com>
2026-08-12 22:32:46 +0530

    Update test_db.py

--- Project-3-The-Data-Warehouse/test_db.py
+++ Project-3-The-Data-Warehouse/test_db.py
@@ -1,5 +1,5 @@
 import pymysql
-conn = pymysql.connect(host='127.0.0.1', port=3306, user='admin', password='vignesh12', database='interns_db')
+conn = pymysql.connect(host='127.0.0.1', port=3306, user='admin', password='vicky1122', database='interns_db')
 cursor = conn.cursor()
 cursor.execute("SELECT * FROM Interns1;")
 for row in cursor.fetchall():
